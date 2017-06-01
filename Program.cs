93535506dfc05d2467b795e91dca63170bedb50e
Ashwin Gatadi <ashwin.gatadi@gmail.com>
2017-06-01 16:48:01 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            int length = arr.Length;
+            SortingAlgorithms sort = new SortingAlgorithms();
+
+
+            //sort.SelectionSort(ref arr, length);
+            //sort.BubbleSort(ref arr, length);
+            sort.InsertionSort(ref arr, length);
+
+
+            Console.WriteLine(string.Join(" ", arr));
+            Console.ReadLine();
+        }
+    }
+}
