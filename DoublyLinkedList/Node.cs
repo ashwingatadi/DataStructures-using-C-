da9e54a814f9033a88c68d9ec0e56b7fbb6c2748
Ashwin Gatadi <ashwin.gatadi@gmail.com>
2017-06-13 15:01:20 -0700

    doubly linked list

---
+++ DoublyLinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    public class Node
+    {
+        public Node Prev { get; set; }
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+}
